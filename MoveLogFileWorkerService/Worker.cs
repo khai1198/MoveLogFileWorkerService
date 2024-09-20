c2518841cdcb46736ccf6578ee7a741b09bb2567
khai <nguyenquangkhai01@gmail.com>
2024-09-20 09:43:44 +0700

    .

--- MoveLogFileWorkerService/Worker.cs
+++ MoveLogFileWorkerService/Worker.cs
@@ -12,53 +12,31 @@ namespace MoveLogFileWorkerService
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            _logger.LogInformation("Start");
             while (!stoppingToken.IsCancellationRequested)
             {
                 try
                 {
-                    _logger.LogInformation("Worker running");
-                    var firstDayOfMonth = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
-                    if (firstDayOfMonth == DateTime.Now.Date)
+                    _logger.LogDebug($"Worker running {HelperConst.SourcePath}");
+                    if (!Directory.Exists(HelperConst.SourcePath))
                     {
-                        _logger.LogInformation("Start move file");
-                        if (!Directory.Exists(HelperConst.SourcePath))
-                        {
-                            _logger.LogError($"SourcePath: {HelperConst.SourcePath} not exists!");
-                            await Task.Delay(TimeSpan.FromHours(6), stoppingToken);
-                            continue;
-                        }
-                        Directory.CreateDirectory(HelperConst.TargetPath);
-                        var files = Directory.GetFiles(HelperConst.SourcePath);
-                        var dateTo = DateTime.Now.Date;
-                        foreach (var file in files)
-                        {
-                            var extention = Path.GetExtension(file);
-                            var fileName = Path.GetFileName(file);
-                            if (extention == ".txt")
-                            {
-                                var date = new DateTime(2000 + int.Parse(fileName.Substring(0, 2)), int.Parse(fileName.Substring(2, 2)), int.Parse(fileName.Substring(4, 2))).Date;
-                                if (date >= dateTo)
-                                {
-                                    continue;
-                                }
-                                var destinationFile = Path.Combine(HelperConst.TargetPath, fileName);
-                                File.Move(file, destinationFile);
-                                _logger.LogInformation("Move file in {pathFrom} to {pathTo}", file, destinationFile);
-                            }
-                            else if (extention == ".csv")
-                            {
-                                var date = new DateTime(int.Parse(fileName.Substring(0, 4)), int.Parse(fileName.Substring(4, 2)), int.Parse(fileName.Substring(6, 2))).Date;
-                                if (date >= dateTo)
-                                {
-                                    continue;
-                                }
-                                var destinationFile = Path.Combine(HelperConst.TargetPath, fileName);
-                                File.Move(file, destinationFile);
-                                _logger.LogInformation("Move file in {pathFrom} to {pathTo}", file, destinationFile);
-                            }
-                        }
+                        await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+                        continue;
                     }
-                    await Task.Delay(TimeSpan.FromDays(1), stoppingToken);
+                    var dir = new DirectoryInfo(HelperConst.SourcePath);
+                    var files = dir.GetFiles().OrderByDescending(x => x.LastWriteTime);
+                    if (files.Count() < 3)
+                    {
+                        await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+                        continue;
+                    }
+                    var fileRemoves = files.Skip(2).ToList();
+                    foreach (var item in fileRemoves)
+                    {
+                        item.Delete();
+                    }
+                    _logger.LogDebug($"Done");
+                    await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
                 }
                 catch (Exception ex)
                 {
